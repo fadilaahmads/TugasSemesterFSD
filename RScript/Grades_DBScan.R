32a170101b7c86ea1feee6794ce5a3b638527103
fadilaahmads <fadilahmad59@gmail.com>
2020-12-17 17:42:59 +0700

    Class.grade menggunakan dbscan

--- RScript/Grades_DBScan.R
+++ RScript/Grades_DBScan.R
@@ -1,29 +1,30 @@
+#Mengimport dataset ke Rstudio
 class.grades <- read.csv("D:/UII/Fundamen Sains Data/Tubes/Tugas Semester FSD/Dataset/class-grades.csv")
+#Melihat isi class.grade.csv
 View(class.grades)
+
+#Tutorial DBSCAN
+#import library
 library("fpc")
+#set seed
 set.seed(123)
+#Menampilkan summary dari class.grade
 summary(class.grades)
-#Assign kolom dari kolom 1 hingga 2 ke var grades
-grades <- class.grades[,1:2]
-#Melihat plot data dengan item berbentuk lingkaran
-plot(grades,pch=16)
 #Assign kolom dari kolom 2 hingga 3 ke var grades
 grades <- class.grades[,2:3]
 #Melihat plot data dengan item berbentuk lingkaran
 plot(grades,pch=16)
-db <- dbscan(grades, eps = 5, MinPts = 5)
+
+db <- dbscan(grades, eps = 6, MinPts = 2)
 plot(db,grades, main = "DBSCAN", frame = FALSE)
+
 #Alternatif plot, dapat menggunakan fungsi untuk plot cluster
 #dari factoextra.
 library(factoextra)
 fviz_cluster(db, grades, stand=FALSE, frame = FALSE, geom = "point")
-db <- dbscan(grades, eps = 15, MinPts = 5)
-fviz_cluster(db, grades, stand=FALSE, frame = FALSE, geom = "point")
-db <- dbscan(grades, eps = 10, MinPts = 3)
-fviz_cluster(db, grades, stand=FALSE, frame = FALSE, geom = "point")
-db <- dbscan(grades, eps = 6, MinPts = 2)
-fviz_cluster(db, grades, stand=FALSE, frame = FALSE, geom = "point")
+
 #Melihat lebih detil dari hasil DBSCAN
 print(db)
+
 #Melihat Cluster dari setiap data point
 db$cluster
